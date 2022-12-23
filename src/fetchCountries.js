e801a04a6592424c23e57f77f9b1eeaddb917d1f
Maksim1903 <dovgusha.m@gmail.com>
2022-12-23 21:02:54 +0100

    .

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,12 @@
-export  default fetchCountries(name) = {
+const endPoint = 'https://restcountries.com/v3.1/name/';
+const nextFields = '?fields=name,capital,flags,languages,population';
 
+export function fetchCountries(name) {
+  const URL_API = '${endPoint}${name}${nextFields}';
+  fetch(URL_API).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json;
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
+import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInformation = document.querySelector('.country-info');
+
+searchBox.addEventListener('input', debounce(inputCountryName, DEBOUNCE_DELAY));
+function inputCountryName(event) {
+  countryInformation.innerHTML = '';
+  const countryName = event.target.value.trim();
+  if (countryName) {
+    fetchCountries(countryName).then(countryCharacteristics).catch(error);
+  }
+}
+
+function countryCharacteristics([
+  { flags, name, capital, population, languages },
+]) {
+  const template = `
+      <div class='main-info'>
+         <img src="${flags.svg}"
+              alt="Flag of Ukraine"
+              class="country-flag"
+              width="40px"
+      />
+         <h1 class="country-name">${name.common}</h1>
+      </div>
+      
+      <p><b>Capital: </b>${capital}</p>
+      <p><b>Population: </b>${population}</p>
+      <p><b>Languages: </b>${Object.values(languages).join(', ')}</p>`;
+
+  console.log(template);
+
+  countryInformation.innerHTML = template;
+}
+
+function error() {
+  (countryInformation.innerHTML = ''),
+    Notify.info('Oops, there is no country with that name');
+}
+
+function checkCountriesData(countryData) {
+  if (countryData < 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  }
+}
